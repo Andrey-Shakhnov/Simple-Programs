8ab4df13d67ea984d902ddc52f9752e12b25dd8e
Andrey <avshakhnov@gmail.com>
2019-09-27 03:43:05 +0300

    Add comment to line 12

--- factorial.py
+++ factorial.py
@@ -9,9 +9,8 @@ N = int(input("Enter N "))
 
 
 def fact(N):
-    if N == 0:
+    if N == 0:  # Factorial of 0 = 1 in definition
         return 1
     return fact(N-1)*N
-
 res2 = fact(N)
 print(res2)
