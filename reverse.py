de02a94ef28d5833f816affd484be8d9da8b3ea0
Andrey <avshakhnov@gmail.com>
2019-09-28 22:42:48 +0300

    Added new way of solution

--- reverse.py
+++ reverse.py
@@ -8,3 +8,9 @@ res1 = ""
 for i in range(len(s1)-1, -1, -1):
     res1 = res1 + s1[i]
 print(res1)
+
+s2 = ['hello', 'man', 'what', 'about', 'session exams?']
+res2 = []
+for i in range(len(s2)):
+    res2.append(''.join(sorted(s2[i])))
+print(res2)
