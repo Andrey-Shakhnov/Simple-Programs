64bfc1509f65cc03044a7123d8a7117a2c330224
Andrey <avshakhnov@gmail.com>
2019-09-28 23:16:28 +0300

    To except special symbols

--- wordcount.py
+++ wordcount.py
@@ -4,16 +4,15 @@ count = 0
 flag = 0
 
 for i in range(len(s)):
-    if s[i] != ' ' and flag == 0:
+    if s[i] != ' ' and flag == 0 and s[i].isalpha() is True:
         count += 1
         flag = 1
     else:
         if s[i] == ' ':
             flag = 0
 
-print(count)
+print(count, "words")
 
 s1 = input("Enter string s1 ")
-s1 = s1.split()
-l = len(s1)
-print(l)
+s1.split()
+print(len(s1), "symbols")
